f69e0a1dc71b50ff15b9841237d94ec8e02072f4
Astrrokid <kidstarro22@gmail.com>
2026-01-04 23:53:19 +0100

    first commit

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from pathlib import Path
+from pydantic.dataclasses import dataclass as pyd_dataclass
+from pydantic import AnyUrl
+
+
+@pyd_dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: str
+    unzip_dir: Path
+
+
+@pyd_dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    update_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
+@pyd_dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    update_base_model_path: Path
+    training_data: Path
+    params_image_size: list
+    params_is_augmentation: bool
+    params_batch_size: int
+    params_epochs: int
+    params_learning_rate: float
