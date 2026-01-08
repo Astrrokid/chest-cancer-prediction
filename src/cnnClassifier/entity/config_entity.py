d08cefeb9ca5772d0dc361743fd191acf2fa8216
Astrrokid <kidstarro22@gmail.com>
2026-01-08 10:20:12 +0100

    dvc aadded

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -35,3 +35,13 @@ class TrainingConfig:
     params_batch_size: int
     params_epochs: int
     params_learning_rate: float
+
+
+@pyd_dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    mlflow_uri: str
+    params_image_size: list
+    params_batch_size: int
